e010f1d0b54290b55cc113643108ae3ed556cedc
Alex Samland <alexandersamland2020@u.northwestern.edu>
2019-08-30 09:56:32 -0500

    Update programmable_smarticle.ino

--- Code/arduino/programmable_smarticle/programmable_smarticle.ino
+++ Code/arduino/programmable_smarticle/programmable_smarticle.ino
@@ -5,24 +5,20 @@
 Smarticle smart(1);
 
 void setup() {
-smart.led_on(1);
-delay(4000);
-smart.led_on(0);
+smart.set_led(1);
+delay(3000);
+smart.set_led(0);
 smart.Xbee.attachInterrupt(handleRxChar);
 smart.Xbee.begin(9600);
 Serial.begin(9600);
 }
 
 void loop() {
-//  int t0 = millis();
-//  if (smart.get_mode()!=0){
-//    smart.read_sensors();
-//    smart.transmit_data();
-//  }
-  if (smart.msg_flag==1){
+ smart.read_sensors();
+ smart.transmit_data();
+ if (smart.msg_flag==1){
     smart.interp_msg();
   }
-//  while (millis()-t0<CYCLE_MS){}
 }
 
 
