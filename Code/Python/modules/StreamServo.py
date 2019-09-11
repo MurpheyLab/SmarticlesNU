91068acd1cf567baa201f8495cc4b4f0b71be937
Alex Samland <alexandersamland2020@u.northwestern.edu>
2019-09-11 12:19:52 -0500

    rename module

--- Code/Python/modules/StreamServo.py
+++ Code/Python/modules/StreamServo.py
@@ -6,10 +6,10 @@
 import threading
 import time
 
-class StreamServoThread(threading.Thread):
+class StreamServo(object):
 
     def __init__(self,xbee,gait_f, frequency = 30, remote_device= None, time_noise= None):
-        if time_noise = None:
+        if time_noise == None:
             self.time_noise = lambda: 0
         self.xb=xbee
         self.thread_flag = threading.Event()
@@ -18,21 +18,25 @@ class StreamServoThread(threading.Thread):
         self.initialize(gait_f,frequency,remote_device)
         self.period_s = round(1/frequency,3)
         self.gait=gait_f
-        super().__init__(target=self.target_function, args=(self.gait, self.period_s, self.dev, self.xb, self.time_noise), daemon = True)
+        self.thread=threading.Thread(target=self.target_function, args=(self.gait, self.period_s, self.dev, self.xb, self.time_noise), daemon = True)
 
     def exit(self):
-    self.exit_flag.set()
+        self.exit_flag.set()
+
+    def initialize_thread(self):
+        self.thread=threading.Thread(target=self.target_function, args=(self.gait, self.period_s, self.dev, self.xb, self.time_noise), daemon = True)
+
 
 
 
     def target_function(self,gaitf,period_s,dev,xb,time_noise):
         t=0
-        while self.exit_flag,is_set() and self.thread_flag.wait():
+        while self.exit_flag.is_set() and self.thread_flag.wait():
             t0 = time.time()
             t_noise = time_noise()
             gait = gaitf(t)-[32,32] #subtract ASCII_OFFSET
             msg = bytearray(gait)+bytearray(0x12)
-            while ((time.time()-t0<period_s+t_noise):
+            while ((time.time()-t0<period_s+t_noise)):
                 pass
             xb.broadcast(msg,remote_device=dev)
             t+=period_s
