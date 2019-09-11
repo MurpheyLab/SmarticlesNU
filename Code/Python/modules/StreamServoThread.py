f6e64d58fc6968517495c55a038b069c8a7ee150
Alex Samland <alexandersamland2020@u.northwestern.edu>
2019-09-11 11:31:54 -0500

    rename StreamServo -> StreamServoThread

--- Code/Python/modules/StreamServoThread.py
+++ Code/Python/modules/StreamServoThread.py
@@ -6,21 +6,19 @@
 import threading
 import time
 
-class StreamServo(Thread):
+class StreamServoThread(threading.Thread):
 
     def __init__(self,xbee,gait_f, frequency = 30, remote_device= None):
-        self.dev = remote_device
-        self.f=frequency
-        self.period_s = round(1/frequency,3)
-        self.gait=gait_f
         self.xb=xbee
         self.thread_flag = threading.event()
+        self.initialize(gait_f,frequency,remote_device)
+        self.period_s = round(1/frequency,3)
+        self.gait=gait_f
+        super().__init__(target=self.target_function, args = (self.gait,self.period_s,self.dev,self.xb), daemon = True)
 
-    def initialize(self):
-        super().__init__(target=self.target_function, args = (self.gait,self.period_s,self.dev), daemon = True)
-        self.event()
 
-    def self.target_function(self,gaitf,period_s,dev):
+
+    def target_function(self,gaitf,period_s,dev,xb):
         t=0
         while self.thread_flag.wait():
             t0 = time.time()
@@ -28,5 +26,5 @@ class StreamServo(Thread):
             msg = bytearray(gait)+bytearray(0x12)
             whiile ((time.time()-t0):
                 pass
-            self.xb.broadcast(msg,remote_device=dev)
+            xb.broadcast(msg,remote_device=dev)
             t+=period_s
