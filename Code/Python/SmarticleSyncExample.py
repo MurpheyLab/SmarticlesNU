ab75ee80d675b29d88f712ec3f13b6951415b81c
Alex Samland <alexandersamland2020@u.northwestern.edu>
2019-09-11 12:20:12 -0500

    example file

--- Code/Python/SmarticleSyncExample.py
+++ Code/Python/SmarticleSyncExample.py
@@ -1,6 +1,7 @@
 #SmarticleSyncExample.py
 
 from modules.SmarticleSwarm import SmarticleSwarm
+from modules.StreamServo import StreamServo
 from random import randint
 import time
 import numpy as np
@@ -44,32 +45,32 @@ def timed_go(min):
     swarm.set_servos(0)
 
 
-#instantiate SmarticleSwarm object with default args
-#change/specify USB port!!
+# instantiate SmarticleSwarm object with default args
+# change/specify USB port!!
 swarm = SmarticleSwarm(port=PORT_NAME)
 swarm.xb.add_rx_callback(rx_callback)
-#discover smarticles on network; by default has 15s timeout
-swarm.build_network(5)
-#change all smarticles to gait interpolate mode
-swarm.set_mode(2)
+# discover smarticles on network; by default has 15s timeout
+# swarm.build_network(5)
+# #change all smarticles to gait interpolate mode
+# swarm.set_mode(2)
+#
+# #send all smarticles the same random gait
+# n=15
+# L = [randint(30,60) for x in range(n)]
+# R = [randint(70,110) for x in range(n)]
+# swarm.gi([L,R],200) #gaits, delay between poitns in ms; I wouldnt go faster than 200ms
 
-#send all smarticles the same random gait
-n=15
-L = [randint(30,60) for x in range(n)]
-R = [randint(70,110) for x in range(n)]
-swarm.gi([L,R],200) #gaits, delay between poitns in ms; I wouldnt go faster than 200ms
-
-#or if I want to send them each a unique random gait
+# or if I want to send them each a unique random gait
 # random_gaits(15, 200)
 
 
-#always call this after updating gait in swarm.gi()
+# always call this after updating gait in swarm.gi()
 swarm.init_sync_thread()
 
-##to start sync:
-#swarm.start_sync()
-##or use
-#timed_sync(minutes)
+## to start sync:
+# swarm.start_sync()
+## or use
+# timed_sync(minutes)
 
 #if you dont want them to be synchronized:
 #swarm.set_servos(1) or timed_go(minutes)
